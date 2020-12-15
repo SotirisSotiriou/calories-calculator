2d7f161f31d011764fa67035b22667d1f9806abb
Sotiris Sotiriou <45819898+SotirisSotiriou@users.noreply.github.com>
2020-12-15 19:57:11 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function calculateCalories(){
+    let caloriesNeeded;
+    let weight = parseFloat(document.getElementById("weight").value);
+    let height = parseInt(document.getElementById("height").value);
+    let age = parseInt(document.getElementById("age").value);
+    let activenessValue = parseInt(document.getElementById("activeness").value);
+    
+    let gender = (document.getElementById("male").checked) ? "Male" : (document.getElementById("female") ? "Female" : null);
+
+    //                  Λ
+    //                  |
+    //                  |
+    //                 ---
+
+    // if(document.getElementById("male").checked == true){
+    //     gender = "Male";
+    // }
+    // else if(document.getElementById("female").checked == true){
+    //     gender = "Female";
+    // }
+
+    if(gender != null){
+        if(gender == "Male"){
+            caloriesNeeded = 10 * weight + 6.25 * height - 5 * age + 5 + activenessValue;
+        }
+        else if(gender == "Female"){
+            caloriesNeeded = 10 * weight + 6.25 * height - 5 * age - 161 + activenessValue; 
+        }
+    
+        document.getElementById("caloriesNeeded").value = caloriesNeeded;
+    }
+}
